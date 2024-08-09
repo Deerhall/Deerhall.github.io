5fcf47a43908ffa3eaca6144c0095589d53b9f03
Deerhall <selimhjorthall@gmail.com>
2024-08-09 10:46:24 +0200

    Template setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	output: "export",
-	// distDir: "dist",
 
+// const isProd = (process.env.NODE_ENV = "production");
+
+const nextConfig = {
+	// basePath: isProd ? "/Deerhall.github.io" : "",
+	// output: "export",
+	// distDir: "dist",
 	// images: {
 	// 	unoptimized: true,
 	// }
